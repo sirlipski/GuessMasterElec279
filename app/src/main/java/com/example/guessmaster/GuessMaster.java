c92469c573bb44f277e01c144a874e24f9322d99
sirlipski <38389415+sirlipski@users.noreply.github.com>
2022-04-04 14:23:03 -0400

    Initial commit

---
+++ app/src/main/java/com/example/guessmaster/GuessMaster.java
@@ -0,0 +1,14 @@
+package com.example.guessmaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GuessMaster extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guess_activity);
+    }
+}
